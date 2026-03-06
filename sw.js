bcef547107227510090f607d46ce1ae3ff89e285
m2sa-labs <m2sa.ppp@gmail.com>
2026-03-06 12:45:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,55 @@
-const CACHE_NAME = "life-log-cache-v4";
+const CACHE_NAME = "life-log-cache-v5";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./icon.png",
-  "./manifest.json",
-  "./sw.js"
+  "./manifest.json"
 ];
 
 // インストール
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// ⭐ 古いキャッシュ削除
+// 古いキャッシュ削除
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys.map(key => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
-      );
-    })
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
-// 通信
+// fetch
 self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put("./index.html", responseClone);
+          });
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
       })
   );
 });
